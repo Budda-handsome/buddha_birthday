c30cecd39aaab5a12ae1c85050e3a0021fee35b1
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-04-28 16:05:42 +0900

    tab change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import styles from "./App.moudle.css";
-import styles2 from "./styles.css";
+
+import styles from "./styles.css";
 import logo from './buddha.png';
+import styled from "styled-components"
 
 import {BrowserRouter as Router,Routes, Route, useNavigate, Link} from "react-router-dom"
 import React, { useState } from "react";
@@ -61,19 +62,19 @@ function SignUp(){
     <div id="tab-1" className="tab-content">
             <form action="" method="get" className="form-signup">
               <div className="form-signup">
-                <label for="name">닉네임 </label>
+                <label htmlFor="name">닉네임 </label>
                 <input type="text" name="name" id="name" placeholder="10자 이하" required/>
                 </div>
               <div className="form-signup">
-                <label for="id">아이디 </label>
+                <label htmlFor="id">아이디 </label>
                 <input type="text" name="id" id="id" placeholder="영문 소문자, 숫자 4~20자" required/>
                 </div>
               <div className="form-signup">
-                <label for="password">비밀번호 </label>
+                <label htmlFor="password">비밀번호 </label>
                 <input type="password" name="password" id="password" placeholder="입력해주세요" required/>
               </div>
               <div className="form-signup">
-                <label for="passwordcheck">비밀번호 확인 </label>
+                <label htmlFor="passwordcheck">비밀번호 확인 </label>
                 <input type="text" name="passwordcheck" id="passwordcheck" placeholder="한번 더 입력해주세요" required/>
                 </div>
 
@@ -84,11 +85,11 @@ function SignUp(){
      <div id="tab-2" className="tab-content">
             <form action="" method="get" className="form-login">
               <div className="form-login">
-                <label for="id">아이디 </label>
+                <label htmlFor="id">아이디 </label>
                 <input type="text" name="id" id="id" required/>
                 </div>
               <div className="form-login">
-                <label for="password">비밀번호 </label>
+                <label htmlFor="password">비밀번호 </label>
                 <input type="password" name="password" id="password" required/>
                 </div>
             </form>
@@ -97,6 +98,11 @@ function SignUp(){
   }
   
  
+  const AdSlideLI = styled.li`
+    color: ${props => (props.active ? `#634646` : `#fff`)};
+    background-color: ${props => (props.active ?  `#fff`: `#634646`)};
+    
+  `;
     return (
       <div className="wrapper">
       <header className="head">
@@ -104,17 +110,14 @@ function SignUp(){
         <p>등불을 밝히고 마음을 나누세요</p>
       </header>
 
-          <div className="container" style= {styles2}>
+          <div className="container">
             <ul className="tabs" >
-              <li onClick={() => clickHandler(0)} className="tab-link " data-tab="tab-1">
-                <input type="radio" id="signup" name="select"></input>
-                <label for="signup">회원가입</label>
-                </li>
-              <li onClick={() => clickHandler(1)}className="tab-link" data-tab="tab-2">
-                  <input type="radio" id="signin" name="select"></input>
-                  <label for="signin">로그인</label>
-              
-              </li>
+              <AdSlideLI active={count === 0} onClick={() => clickHandler(0)} className="tab-link ">
+                <span>회원가입</span>
+                </AdSlideLI>
+                <AdSlideLI active={count=== 1} onClick={() => clickHandler(1)}className="tab-link">
+                  <span>로그인</span>
+              </AdSlideLI>
               
             </ul>
 
